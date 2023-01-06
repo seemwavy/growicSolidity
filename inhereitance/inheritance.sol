802dc2b70e1cd58bb250980498b19160c10e0198
seemwavy <seemwavy@gmail.com>
2023-01-06 15:00:55 -0800

    create inheritance contract and add it's dependencies

---
+++ inhereitance/inheritance.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import "./erc20.sol";
+
+contract inheritance is ERC20 {
+    constructor(uint256 initialSupply) ERC20("WavyToken", "WAVY") {
+        // I could hard code it like this :initialSupply = 5000000;
+        // But using initialSupply as a variable I can choose the number during deployment.
+        _mint(msg.sender, initialSupply);
+    }
+}
