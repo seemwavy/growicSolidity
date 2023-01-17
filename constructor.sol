afe7ad41a069c523c6a6452448bfc89c2a1ae580
seemwavy <seemwavy@gmail.com>
2023-01-16 21:11:22 -0800

    add getter to contructor contract

--- constructor.sol
+++ constructor.sol
@@ -1,21 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-
 contract A {
     address public owner;
     uint public FEE;
 
     constructor (uint _fee) {
         owner = msg.sender;
-        _fee = FEE;
+        FEE = _fee;
     }
 }
 
-
-contract B is A{
-    address public owner;
-    constructor (uint _fee) A(20) {
+contract B is A {
+    constructor (uint _fee) A(_fee) {
         owner = msg.sender;
     }
-
+    function getFee() public view returns (uint) {
+        return FEE;
+    }
 }
